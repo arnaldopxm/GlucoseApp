369cf77ae58e1bcd4f4f901fad89bd0d6dc6bd1b
Arnaldo Quintero <arnaldoquinteo96@gmail.com>
2022-02-03 21:31:17 +0000

    fix views on watch

--- GlucoseApp WatchKit Extension/Views/ContentView.swift
+++ GlucoseApp WatchKit Extension/Views/ContentView.swift
@@ -6,17 +6,30 @@
 //
 
 import SwiftUI
+import GlucoseAppHelper
 
 struct ContentView: View {
-    @StateObject var model = ViewModelWatch.singleton
+    @StateObject var state = WatchState.singleton
+    
     var body: some View {
         VStack {
-            Text(model.sg)
-                .padding()
-            Text(model.sgTime)
-                .padding()
-            Text(model.nextUpdateTime)
-                .padding()
+            Spacer()
+            HStack {
+                Spacer()
+            }
+            StylesConst.GlucoseShowCircle(
+                sgColor: state.sgColor,
+                sg: state.sg,
+                sgTrend: state.sgTrend
+            )
+                .scaleEffect(0.5)
+                .frame(width: 140, height: 140, alignment: .center)
+            Spacer().frame(height: 10)
+            Text(AppState.getDateFormated(state.sgTime))
+                .modifier(ViewModifiers.GlucoseAppTextStyle(height: 21))
+            Text("Hace \(SensorGlucose.getTimeOffsetInMinutes(from: state.sgTime) ?? 0) min." )
+                .modifier(ViewModifiers.GlucoseAppTextStyle(fontSize: 15, color: ColorsConst.TEXT_COLOR_SECONDARY, height: 19))
+            
         }
     }
 }
@@ -24,7 +37,8 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ContentView()
+            ContentView(state: WatchState.sample_ok_notrend)
+            ContentView(state: WatchState.sample_ok_up)
         }
     }
 }
